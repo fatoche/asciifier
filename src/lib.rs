8eec23e35fb0b29f374b1d130ab66e791f05f8f0
Clara Scherer <git20357@s.cherer.de>
2018-05-30 23:57:05 +0200

    put everything into a module

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,36 @@
-use std::collections::HashMap;
+pub mod asciifier {
 
-pub struct Asciifier {
-    character_map: HashMap<char, &'static str>,
+    use std::collections::HashMap;
+
+    pub struct Asciifier {
+        character_map: HashMap<char, &'static str>,
+    }
+
+    impl Asciifier {
+
+        pub fn new() -> Asciifier {
+            let mut char_map = HashMap::with_capacity(20);
+            char_map.insert('ä', "ae");
+            char_map.insert('ö', "oe");
+            char_map.insert('ü', "ue");
+            char_map.insert('Ä', "Ae");
+            char_map.insert('Ö', "Oe");
+            char_map.insert('Ü', "Ue");
+            char_map.insert('ß', "ss");
+            Asciifier {
+                character_map: char_map,
+            }
+        }
+
+        pub fn to_ascii(&self, from: String) -> String {
+            let mut to = from;
+            for (special_char, replacement) in &self.character_map {
+                to = to.replace(*special_char, replacement);
+            }
+            to
+        }
 }
 
-impl Asciifier {
-
-    pub fn new() -> Asciifier {
-        let mut char_map = HashMap::with_capacity(20);
-        char_map.insert('ä', "ae");
-        char_map.insert('ö', "oe");
-        char_map.insert('ü', "ue");
-        char_map.insert('Ä', "Ae");
-        char_map.insert('Ö', "Oe");
-        char_map.insert('Ü', "Ue");
-        char_map.insert('ß', "ss");
-        Asciifier {
-            character_map: char_map,
-        }
-    }
-
-    pub fn to_ascii(&self, from: String) -> String {
-        let mut to = from;
-        for (special_char, replacement) in &self.character_map {
-            to = to.replace(*special_char, replacement);
-        }
-        to
-    }
 }
 
 
